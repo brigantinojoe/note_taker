8a44075e7f3b9c5fac335cc867db805db0a85dc6
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-12-10 15:39:06 -0800

    Add uuid package to package.json file. Add get route to send client to /notes.html page once clicking the 'Get Started' button. Create GET route to render notes from db.json file. Create POST route to save the new notes to the db.json file and add id.

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
 
 const PORT = 3001;
 
@@ -15,20 +17,47 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Routes
-    // Get /api/notes
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-        console.log(data);
-    });
-});
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-    // Post /api/notes
-    // delete api/notes/${id}
+
+// Create Get /api/notes route
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        var newData = JSON.parse(data);
+        res.json(newData);
+    });
+});
+
+// Post /api/notes
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        let notesDb = JSON.parse(data);
+        let newNote = req.body;
+        newNote.id = uuidv4();
+        notesDb.push(newNote);
+        console.log(notesDb);
+        const allNotes = JSON.stringify(notesDb);
+        fs.writeFile('./db/db.json', allNotes, (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+              `New note has been added to the database.`
+          )
+        );
+    });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// delete api/notes/${id}
+// Read all notes from db.json
+// Remove note with given id
+// Rewrite notes to db.json
 
 // Port
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
